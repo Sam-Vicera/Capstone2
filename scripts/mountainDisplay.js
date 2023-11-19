71841996f2cb80da1506685bb843fde3535ce6b3
Sam-Vicera <shernandezvicera@gmail.com>
2023-11-19 10:52:17 -0700

    mountain info displayed

--- scripts/mountainDisplay.js
+++ scripts/mountainDisplay.js
@@ -26,13 +26,10 @@ function createContainer(){
      
     let selectedMountainArray = mountainsArray.find(mountain => mountain.name == selectedMountain);
 
-    console.log(selectedMountainArray);
+    
      displayContent(selectedMountainArray);
     }
 
-    else {
-        console.log("Select your Mountain")
-    }
 }
 
 
@@ -51,6 +48,45 @@ col1.classList.add("col-5");
 col1.classList.add("mt-5");
 rowDiv.appendChild(col1);
 
+let infoDiv = document.createElement("div");
+col1.appendChild(infoDiv);
+
+let headingDiv = document.createElement("h5");
+headingDiv.innerText = mountainArray.name
+infoDiv.appendChild(headingDiv);
+
+let paragraph1 = document.createElement("p");
+let span1 = document.createElement("span");
+span1.innerText = "Elevation: ";
+paragraph1.appendChild(span1);
+paragraph1.innerText += `${mountainArray.elevation}`
+infoDiv.appendChild(paragraph1);
+
+
+
+let paragraph2 = document.createElement("p");
+let span2 = document.createElement("span");
+span2.innerText = "Effort: "
+paragraph2.appendChild(span2)
+paragraph2.innerText += `${mountainArray.effort}`
+infoDiv.appendChild(paragraph2);
+
+let paragraph3 = document.createElement("p");
+let span3 = document.createElement("span");
+span3.innerText = "Description: ";
+paragraph3.appendChild(span3);
+paragraph3.innerText += `${mountainArray.desc}`
+infoDiv.appendChild(paragraph3);
+
+let paragraph4 = document.createElement("p");
+let span4 = document.createElement("span");
+span4.innerText = "Coordinates: ";
+paragraph4.appendChild(span4);
+paragraph4.innerText += ` Latitude: ${mountainArray.coords.lat} Longtitude: ${mountainArray.coords.lng} `
+infoDiv.appendChild(paragraph4);
+
+
+
 let col2 = document.createElement("div");
 col2.classList.add("col");
 col2.classList.add("mt-5");
@@ -58,7 +94,6 @@ col2.classList.add("ms-5");
 rowDiv.appendChild(col2);
 
 let imgDiv = document.createElement("img");
-console.log(mountainArray.name)
  imgDiv.src = "images/"+ mountainArray.img ;
 col2.appendChild(imgDiv);
 }
