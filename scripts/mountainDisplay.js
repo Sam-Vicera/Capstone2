fc9c86965801325cd61efd6acab148822c6394f1
Sam-Vicera <shernandezvicera@gmail.com>
2023-11-18 23:57:24 -0700

    Tried using filtering because I didn't understand find-search, understand it now and will replace this code

--- scripts/mountainDisplay.js
+++ scripts/mountainDisplay.js
@@ -1,26 +1,64 @@
 
 document.addEventListener("DOMContentLoaded",()=>{
 
-    const mountainSelect = document.getElementById("mountain-select");
+    const mountainSelectList = document.getElementById("mountain-select");
      
-    let mountainsNameArray = mountainsArray.map(m => m.name);
+    const newNameArray = mountainsArray.map(m => m.name);
      
 
     
-    mountainsNameArray.forEach(namesArray => {
-       
+    newNameArray.forEach(namesArray => {
        const mountainChoice = new Option(namesArray);
-       mountainSelect.add(mountainChoice)
+       mountainSelectList.add(mountainChoice);
     } )
 
-    mountainSelect.addEventListener("change", displayMountains)
+   
+
+    mountainSelectList.addEventListener("change", createContainer);
+    
+    createContainer();
 })
 
-function displayMountains(){
-    const mountainsContainer = document.getElementById("mountainDisplay1");
+function createContainer(){
+     let selectedMountain = document.getElementById("mountain-select").value
 
-    mountainsContainer.innerText = "";
+    if(selectedMountain != "Select your Mountain"){
+    let selectedMountainArray = mountainsArray.filter(mountain => mountain.name.includes(selectedMountain));
+     console.log(selectedMountainArray);
+     displayContent(selectedMountainArray);
+    }
+    else {
+        console.log("Select your Mountain")
+    }
 
+    
 
+  
 }
 
+function displayContent(mountainArray){
+    const mountainContainer = document.getElementById("mountainDisplay1");
+    mountainContainer.innerText = "";
+
+ mountainContainer.classList.add("w-75");
+
+let rowDiv = document.createElement("div");
+rowDiv.classList.add("row");
+mountainContainer.appendChild(rowDiv);
+
+let col1 = document.createElement("div");
+col1.classList.add("col-5");
+col1.classList.add("mt-5");
+rowDiv.appendChild(col1);
+
+let col2 = document.createElement("div");
+col2.classList.add("col");
+col2.classList.add("mt-5");
+col2.classList.add("ms-5");
+rowDiv.appendChild(col2);
+
+let imgDiv = document.createElement("img");
+console.log(mountainArray.name)
+ imgDiv.src = "images/"+ mountainArray.name ;
+col2.appendChild(imgDiv);
+}
